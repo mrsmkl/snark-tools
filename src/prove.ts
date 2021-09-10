358febed52b927fe235ed13f3ecbe30094152067
Sami Mäkelä <mrsmkl@gmail.com>
2021-09-10 15:13:02 +0300

    fixing

--- src/prove.ts
+++ src/prove.ts
@@ -4,6 +4,7 @@ import { utils } from '@nevermined-io/nevermined-sdk-js'
 const { makeKey, secretToPublic, makePublic, encryptKey, ecdh, prove, hashKey } = utils
 
 async function main() {
+    console.error(process.argv)
     let providerK = makeKey(process.argv[2])
     let providerPub = secretToPublic(providerK)
     let data = Buffer.from(process.argv[3].substr(2), 'hex')
@@ -11,9 +12,8 @@ async function main() {
     const cipher = encryptKey(data, ecdh(providerK, buyerPub))
     const proof = await prove(buyerPub, providerPub, providerK, data)
     const hash = hashKey(data)
-    console.log("HASH", hash)
-    console.log("CIPHER", cipher.x, cipher.y)
-    console.log("PROOF", proof)
+    const res = { hash, cipher: [cipher.x, cipher.y], proof }
+    console.log(JSON.stringify(res))
     process.exit(0)
 }
 
